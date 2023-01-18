8884889afa45bef9cb51f6c66d3c115a76e0a196
Jainis Talaviya <jainis.talaviya@brevitaz.com>
2023-01-18 18:00:22 +0530

    stage1: Initial Commit

---
+++ src/com/brevitaz/stage1/Main.java
@@ -0,0 +1,9 @@
+package com.brevitaz.stage1;
+
+public class Main {
+	public static void main(String[] args) {
+		TheatreManagement theatre=new TheatreManagement();
+		theatre.createHall();
+		theatre.showHall();
+	}
+}
---
+++ src/com/brevitaz/stage1/TheatreManagement.java
@@ -0,0 +1,46 @@
+package com.brevitaz.stage1;
+
+public class TheatreManagement {
+	int hall[][];
+	int rowSize=7,colmSize=8,row,colm;
+
+	public void createHall() {
+
+		hall = new int[(rowSize + 1)][(colmSize + 1)];
+
+		for (row = 0; row < rowSize + 1; row++) {
+			for (colm = 0; colm < (colmSize + 1); colm++) {
+				if (row == 0) {
+					int temp = (colm == 0) ? ' ' : colm;
+					hall[row][colm] = temp;
+				} else if (colm == 0) {
+					hall[row][colm] = row;
+				} else {
+					hall[row][colm] = 'S';
+				}
+			}
+		}
+	}
+	public void showHall()
+	{
+		System.out.println("cinema:");
+		for (row=0;row<rowSize+1;row++)
+		{
+			for (colm=0;colm<colmSize+1;colm++)
+			{
+				if(row==0)
+				{
+					System.out.print((colm==0)?(char)hall[row][colm] + " ":hall[row][colm] + " ");
+				}
+				else if(colm==0)
+				{
+					System.out.print(hall[row][colm] + " ");
+				}
+				else {
+					System.out.print((char) hall[row][colm] + " ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
