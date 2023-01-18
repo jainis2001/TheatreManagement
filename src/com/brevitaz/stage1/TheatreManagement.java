e67fe40fa2caa7399a09947a9c3700c4d3233224
Jainis Talaviya <jainis.talaviya@brevitaz.com>
2023-01-18 18:58:01 +0530

    stage 2: create hall according user input and get total income

--- src/com/brevitaz/stage1/TheatreManagement.java
+++ src/com/brevitaz/stage1/TheatreManagement.java
@@ -1,24 +1,57 @@
 package com.brevitaz.stage1;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class TheatreManagement {
 	int hall[][];
-	int rowSize=7,colmSize=8,row,colm;
+	int rowSize,colmSize,row,colm;
+
+	public int[][] getHall() {
+		return hall;
+	}
 
 	public void createHall() {
+		Scanner scanner=new Scanner(System.in);
+		try{
+			System.out.println("Enter the number of rows:");
+			rowSize=scanner.nextInt();
+			System.out.println("Enter the number of seats in each row:");
+			colmSize=scanner.nextInt();
 
-		hall = new int[(rowSize + 1)][(colmSize + 1)];
+			hall = new int[(rowSize + 1)][(colmSize + 1)];
+			if((rowSize<=0 || rowSize>9) || (colmSize<=0 || colmSize >9) ){
+				System.err.println("row or seats range must between 1 to 9..");
+				createHall();
 
-		for (row = 0; row < rowSize + 1; row++) {
-			for (colm = 0; colm < (colmSize + 1); colm++) {
-				if (row == 0) {
-					int temp = (colm == 0) ? ' ' : colm;
-					hall[row][colm] = temp;
-				} else if (colm == 0) {
-					hall[row][colm] = row;
-				} else {
-					hall[row][colm] = 'S';
+			}
+			else {
+				for (row = 0; row < rowSize + 1; row++) {
+					for (colm = 0; colm < (colmSize + 1); colm++) {
+						if (row == 0) {
+							int temp = (colm == 0) ? ' ' : colm;
+							hall[row][colm] = temp;
+						} else if (colm == 0) {
+							hall[row][colm] = row;
+						} else {
+							hall[row][colm] = 'S';
+						}
+					}
 				}
 			}
+
+
+
+		}
+		catch(InputMismatchException e)
+		{
+			System.err.println("Only Integer input allowed");
+			createHall();
+		}
+		catch (Exception e)
+		{
+			System.out.println("Wrong Input:");
+			createHall();
 		}
 	}
 	public void showHall()
---
+++ src/com/brevitaz/stage2/Statistics.java
@@ -0,0 +1,27 @@
+package com.brevitaz.stage2;
+
+
+
+public class Statistics {
+	int total,firstHalfRow,secondHalfRow,firstHalfCount,secondHalfCount;
+	public void getTotalIncome(int rowSize,int colmSize)
+	{
+//		System.out.println(rowSize+" "+colmSize);
+		total=rowSize*colmSize;
+		if(total>0 &&total<60)
+		{
+			total=rowSize*(colmSize*10);
+		}
+		else{
+			firstHalfRow=rowSize/2;
+			secondHalfRow=rowSize-firstHalfRow;
+
+			firstHalfCount = firstHalfRow *(colmSize*10) ;
+			secondHalfCount=secondHalfRow*(colmSize*8) ;
+			total=firstHalfCount+secondHalfCount;
+
+		}
+		System.out.println("\nTotal Income:\n$"+total);
+
+	}
+}
