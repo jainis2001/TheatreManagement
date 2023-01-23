175fe439efbd5981b7cee4a70433ed8eb3f43b53
Jainis Talaviya <jainis.talaviya@brevitaz.com>
2023-01-23 11:41:51 +0530

    try

--- src/com/brevitaz/stage2/Main.java
+++ src/com/brevitaz/stage2/Main.java
@@ -1,9 +1,10 @@
 package com.brevitaz.stage2;
 
 import com.brevitaz.stage1.TheatreManagement;
-
+//comment
 public class Main {
 	public static void main(String[] args) {
+
 		int tempRowSize=0,tempColmSize=0;
 		TheatreManagement theatre=new TheatreManagement();
 		theatre.createHall();
