d1c58e3cbbc0456f13cb2b13b85828de2d11edb1
Jainis Talaviya <jainis.talaviya@brevitaz.com>
2023-01-30 17:01:51 +0530

    theatre: changes commited

--- src/com/brevitaz/Theatre/BookSeat.java
+++ src/com/brevitaz/Theatre/BookSeat.java
@@ -1,72 +1,71 @@
 package com.brevitaz.Theatre;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class BookSeat{
+public class BookSeat {
 	private int rowSize;
 	private int colmSize;
 	private int ticketPrice;
+	private int row, colm;
+	private char seats[][];
+	private Scanner scan;
 
-	// getBooked(char[][] seats) will take array and booked the particular seat according the user input
-	// according the chosen seat will show ticket price
-	public void getBooked(char[][] seats)
-	{
-		//inital comment
+	public void initSeats(char[][] seats) {
+		this.seats = seats;
+		rowSize = seats.length;
+		colmSize = seats[0].length;
+		getInput();
+	}
 
-		int totalRoomSeats, firstHalfRow, priceLS60 = 10, priceGT60FirstHalf = 10, priceGT60SecondHalf = 8;
-		Scanner scan = new Scanner(System.in);
+	public void getInput() {
+		scan = new Scanner(System.in);
 		try {
 
 			System.out.println("Enter number of row:");
-			int row = scan.nextInt();
+			row = scan.nextInt();
 			System.out.println("Enter seat number in " + (row) + " row:");
-			int colm = scan.nextInt();
-
-			rowSize=seats.length;
-			colmSize=seats[0].length;
-			firstHalfRow=rowSize/2;
-
-			totalRoomSeats=rowSize*colmSize;
-			if(seats[(row -1)][(colm -1)]=='B'){
-				System.err.println("That ticket has already been purchased!");
+			colm = scan.nextInt();
+			if ((row < 1 || row > 10) || (colm < 1 || colm > 10)) {
+				System.err.println("row and seat should be till 1 to " + rowSize + " and " + colmSize);
+				System.out.println("Enter again");
 				scan.reset();
-				getBooked(seats);
-			}
-			else if(totalRoomSeats>0 &&totalRoomSeats<60) {
-				ticketPrice=priceLS60;
-			}
-			else if(row <=firstHalfRow) {
-				ticketPrice=priceGT60FirstHalf;
-			}
-			else{
-			ticketPrice=priceGT60SecondHalf;
-			}
-			seats[(row -1)][(colm -1)]='B';
-
-			System.out.println("\nTicket Price:\n$"+ticketPrice);
-
-		}
-		catch (InputMismatchException i)
-		{
+				getInput();
+			} else
+				bookTicket();
+		} catch (Exception e) {
 			System.err.println("Only Integer Input Allowed");
 			System.out.println("Enter again");
 			scan.reset();
-			getBooked(seats);
+			getInput();
 		}
-		catch (IndexOutOfBoundsException bound)
-		{
-			System.err.println("row and seat should be till 1 to "+rowSize+" and "+colmSize);
-			System.out.println("Enter again");
+
+	}
+
+	// getBooked(char[][] seats) will take array and booked the particular seat according the user input
+	// according the chosen seat will show ticket price
+	public void bookTicket() {
+		//inital comment
+		int totalRoomSeats, firstHalfRow, priceLS60 = 10, priceGT60FirstHalf = 10, priceGT60SecondHalf = 8;
+
+		firstHalfRow = rowSize / 2;
+
+		totalRoomSeats = rowSize * colmSize;
+
+		if (seats[(row - 1)][(colm - 1)] == 'B') {
+			System.err.println("That ticket has already been purchased!");
 			scan.reset();
-			getBooked(seats);
+			getInput();
+		} else if (totalRoomSeats > 0 && totalRoomSeats < 60) {
+			ticketPrice = priceLS60;
+		} else if (row <= firstHalfRow) {
+			ticketPrice = priceGT60FirstHalf;
+		} else {
+			ticketPrice = priceGT60SecondHalf;
 		}
-		catch (Exception e)
-		{
-			System.err.println("Something went wrong..");
-			System.out.println("Enter again");
-			scan.reset();
-			getBooked(seats);
-		}
+		seats[(row - 1)][(colm - 1)] = 'B';
+
+		System.out.println("\nTicket Price:\n$" + ticketPrice);
+
+
 	}
 }
--- src/com/brevitaz/Theatre/Menu.java
+++ src/com/brevitaz/Theatre/Menu.java
@@ -1,2 +1,56 @@
-package com.brevitaz.Theatre;public class Menu {
+package com.brevitaz.Theatre;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Menu {
+
+	private int choice;
+	private final BookSeat book = new BookSeat();
+	private final TheatreManagement theatre = new TheatreManagement();
+	private boolean isFirstCall = true;
+
+	//menu() will allow you to take choice and get output according...
+	public void menu() {
+		Scanner scanner = new Scanner(System.in);
+		if (isFirstCall) {
+			theatre.initRoom();
+		}
+
+		do {
+			try {
+				System.out.println("1. Show the seats");
+				System.out.println("2. Buy a ticket");
+				System.out.println("3. Statistic");
+				System.out.println("0. Exit");
+				System.out.println("Enter Choice:");
+				choice = scanner.nextInt();
+
+				switch (choice) {
+
+					case 1:
+						theatre.dispRoom();
+						break;
+					case 2:
+						book.initSeats(theatre.getRoom());
+						break;
+					case 3:
+						Statistics stat = new Statistics();
+						stat.getStatisticDetails(theatre.getRoom());
+						break;
+					case 0:
+						break;
+					default:
+						System.out.println("Invalid choice:");
+				}
+			} catch (InputMismatchException i) {
+				System.err.println("Something Went Wrong!");
+				isFirstCall = false;
+				scanner.reset();
+				menu();
+			}
+
+		} while (choice != 0);
+
+	}
 }
